93029923cae9622a3955eb189745e339ad7dff30
Artem Shcherbak <artem_shcherbak@mail.ru>
2026-02-25 13:53:50 +0500

    fix

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,27 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldCalculateRemainWhenAmount900() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnZeroWhenAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
